26143869eb694465d5cb45f1cfe42c3db6c2c6e7
Recep Cafer Yavuz <cafer.yavuz@etiya.com>
2021-12-15 17:08:25 +0300

    15.12.2021

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, MongoLogger, ElasticLogger} from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -7,11 +8,23 @@ console.log("user component yüklendi")
 let loggerType = new ElasticLogger()
 let userService = new UserService(loggerType)
 
-let user1 = new User(1,"Recep","Yavuz","İstanbul")
+// let user1 = new User(1,"Recep","Yavuz","İstanbul")
 
-let user2 = new User(2,"Emre","Demir","Ankara")
+// let user2 = new User(2,"Emre","Demir","Ankara")
 
-userService.add(user1)
-userService.add(user2)
-console.log(userService.list())
-console.log(userService.getById(2))
+// userService.add(user1)
+// userService.add(user2)
+// console.log(userService.list())
+// console.log(userService.getById(2))
+
+console.log(userService.load())
+
+let customerToAdd = new Customer(7,"Mahmut","Alaca","Bursa","aa")
+customerToAdd.type = "customer"
+userService.add(customerToAdd)
+
+console.log(userService.customers)
+console.log(userService.employee)
+console.log(userService.errors)
+
+console.log(userService.getEmployeeSorted())
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,18 +1,124 @@
+import { users } from "../data/users.js"
+import DataError from "../models/dataError.js"
+
 export default class UserService{
     constructor(loggerService){
-        this.users=[]
+        this.employee = []
+        this.customers = []
+        this.errors = []
         this.loggerService = loggerService
     }
+
+    load(){
+        for (const user of users) {
+            switch (user.type) {
+                case "customer":
+                    if(!this.checkCustomerValidityForErrors(user)){
+                        this.customers.push(user)
+                    }
+                    break;
+                case "employee":          
+                    if(!this.checkEmployeeValidityForErrors(user)){
+                        this.employee.push(user)
+                    }
+                    break;
+                default:
+                    this.errors.push(new DataError("Wrong user type",user))
+                    break;
+            }
+            console.log(user)
+        }
+    }
+
     add(user){
-        this.users.push(user)
+        switch (user.type) {
+            case "customer":
+                if(!this.checkCustomerValidityForErrors(user)){
+                    this.customers.push(user)
+                }
+                break;
+            case "employee":
+                if(!this.checkEmployeeValidityForErrors(user)){
+                    this.employee.push(user)
+                }
+                break;
+            default:
+                this.errors.push(new DataError("This user can not be added. Wrong user type",user))
+                break;
+        }
         this.loggerService.log(user +" eklendi")
     }
 
-    list(){
-        return this.users
+    //react formik-yup
+    checkCustomerValidityForErrors(user){
+        let requiredField = "id firstName lastName age city".split(" ")
+        let hasErrors = false
+        for (const field of requiredField) {
+            if(!user[field]){
+                hasErrors = true
+                this.errors.push(new DataError(`Validation problem. ${field} is required`,user))
+            }
+        }
+        return hasErrors
     }
 
-    getById(id){
-        return this.users.find(u=> u.id === id)
+    checkEmployeeValidityForErrors(user){
+        let requiredField = "id firstName lastName age city salary".split(" ")
+        let hasErrors = false
+        for (const field of requiredField) {
+            if(!user[field]){
+                hasErrors = true
+                this.errors.push(new DataError(`Validation problem. ${field} is required`,user))
+            }
+        }
+        if(Number.isNaN(Number.parseInt(+user.age))){
+            hasErrors = true
+            this.errors.push(new DataError(`Validation problem. ${user.age} is not a number`,user))
+        }
+        return hasErrors
+    }
+
+    listCustomers(){
+        return this.customers
+    }
+
+    getCustomerById(id){
+        return this.customers.find(u=> u.id === id)
+    }
+    
+    getCustomersSorted(){
+        this.customers.sort((customer1,customer2) => {
+            if(customer1.firstName < customer2.firstName){
+                return 1
+            }
+            else if(customer1.firstName === customer2.firstName){
+                return 0
+            }
+            else{
+            return -1
+            }
+        })
+    }
+
+    listEmployee(){
+        return this.employee
+    }
+
+    getEmployeeById(id){
+        return this.employee.find(u=> u.id === id)
+    }
+    
+    getEmployeeSorted(){
+        return this.employee.sort((employee1,employee2) => {
+            if(employee1.firstName < employee2.firstName){
+                return -1
+            }
+            else if(employee1.firstName === employee2.firstName){
+                return 0
+            }
+            else{
+                return 1
+            }
+        })
     }
 }
